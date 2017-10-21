84686113d9bb328675313ac218bbbcc221ae2bc7
Matthew Machuga <machuga@gmail.com>
2017-10-21 07:12:16 -0400

    First version with archive and links

--- config.rb
+++ config.rb
@@ -36,6 +36,50 @@ page '/*.txt', layout: false
 #     'Helping'
 #   end
 # end
+helpers do
+  def mailing_list_url
+    "//eriedayofcode.us10.list-manage.com/subscribe/post" \
+      "?u=3fcd98d64025f034a082fe384&id=bb907d9e18"
+  end
+
+  def cfp_url
+    'https://goo.gl/forms/hPzcNskx4kLMx6ig1'
+  end
+
+  def tickets_url
+    "https://eriedayofcode.ticketleap.com/2018/dates/May-16-2018_at_0900AM"
+  end
+
+  def social_links
+    OpenStruct.new(
+      twitter: data.info.twitter_url,
+      facebook: data.info.facebook_url
+    )
+  end
+
+  def venue
+    OpenStruct.new(
+      name: 'Erie Art Museum',
+      url: 'http://erieartmuseum.org',
+      street: '411 State St.',
+      address_line: 'Erie, PA 16501',
+    )
+  end
+
+  def close_lodging
+    data.lodging.select { |place| place.proximity == 'close' }
+  end
+
+  def further_lodging
+    data.lodging.select { |place| place.proximity == 'far' }
+  end
+
+  def md_text(text)
+    Kramdown::Document.new(text).to_html
+  end
+end
+
+
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
